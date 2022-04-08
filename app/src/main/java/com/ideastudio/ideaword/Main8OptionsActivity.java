0ccc70ed141bd51930052a182f7314ff1a36fd54
Taido0220 <taido0220@gmail.com>
2022-04-08 14:53:47 +0700

    xong phan offline

--- app/src/main/java/com/ideastudio/ideaword/Main8OptionsActivity.java
+++ app/src/main/java/com/ideastudio/ideaword/Main8OptionsActivity.java
@@ -3,13 +3,19 @@ package com.ideastudio.ideaword;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class Main8OptionsActivity extends AppCompatActivity {
     Button button1;
+    MediaPlayer mediaPlayer;
+    Switch switch1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +26,21 @@ public class Main8OptionsActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent i =new Intent(Main8OptionsActivity.this,Main2TrangchuActivity.class);
                 startActivity(i);
+            }
+        });
+        mediaPlayer = MediaPlayer.create(Main8OptionsActivity.this,R.raw.song);
+        switch1=(Switch) findViewById(R.id.switch1);
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    Toast.makeText(Main8OptionsActivity.this,"Bat",Toast.LENGTH_SHORT).show();
+                    mediaPlayer.start();
+                }else{
+                    Toast.makeText(Main8OptionsActivity.this,"Tat",Toast.LENGTH_SHORT).show();
+                    mediaPlayer.pause();
+                }
+
             }
         });
     }
