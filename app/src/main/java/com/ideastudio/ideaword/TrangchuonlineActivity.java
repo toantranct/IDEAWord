2e50b0fb1e7ada4a8d71837759b3136a3df661d8
Taido0220 <taido0220@gmail.com>
2022-04-08 20:20:28 +0700

    Them giao dien dang nhap

---
+++ app/src/main/java/com/ideastudio/ideaword/TrangchuonlineActivity.java
@@ -0,0 +1,14 @@
+package com.ideastudio.ideaword;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrangchuonlineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trangchuonline);
+    }
+}
