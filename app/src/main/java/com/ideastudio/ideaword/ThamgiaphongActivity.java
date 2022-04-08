1ddfbedbea84f2587f3ff37761bb02884a502804
Taido0220 <taido0220@gmail.com>
2022-04-08 23:22:47 +0700

    Hoàn thành giao diện online

---
+++ app/src/main/java/com/ideastudio/ideaword/ThamgiaphongActivity.java
@@ -0,0 +1,14 @@
+package com.ideastudio.ideaword;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThamgiaphongActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thamgiaphong);
+    }
+}
